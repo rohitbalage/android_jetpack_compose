64545afd94b090ae84907d9a7d895fae20ad43c2
Rohit Balage <thatcomputerboy26@gmail.com>
2024-03-05 11:42:31 -0500

    added text composables

--- app/src/main/java/com/rrbofficial/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/rrbofficial/jetpackcompose/MainActivity.kt
@@ -3,13 +3,17 @@ package com.rrbofficial.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.rrbofficial.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,9 +25,15 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                )
+
+                {
+                    Column{
+                        Greeting("Android")
+                        DisplayOurText()
+                    }
                 }
+
             }
         }
     }
@@ -32,7 +42,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DisplayOurText()
 {
+    // TO display text from string resource
+// Text(text = stringResource(id = R.string.text_src) )
 
+    Text(text = "HELLO EVERYONE"
+    ,
+        color = Color.Blue,
+        fontSize = 48.sp
+    )
 }
 
 @Composable
