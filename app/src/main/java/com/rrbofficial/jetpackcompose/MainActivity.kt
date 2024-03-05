70528842d02767cc06a325a17afb66b1f9e41a4d
Rohit Balage <thatcomputerboy26@gmail.com>
2024-03-05 11:52:07 -0500

    added custom text

--- app/src/main/java/com/rrbofficial/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/rrbofficial/jetpackcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,6 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.rrbofficial.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -45,11 +52,26 @@ fun DisplayOurText()
     // TO display text from string resource
 // Text(text = stringResource(id = R.string.text_src) )
 
-    Text(text = "HELLO EVERYONE"
-    ,
-        color = Color.Blue,
-        fontSize = 48.sp
-    )
+
+    // selectionContainer will enable copy the text in app
+    SelectionContainer{
+
+        Text(text = "HELLO EVERYONE \n WELCOME TO THE JETPACK APP"
+            ,
+            color = Color.Blue,
+            fontSize = 20.sp,
+            style = TextStyle(textDecoration = TextDecoration.LineThrough)
+            ,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.ExtraBold,
+            lineHeight = 22.sp,
+            letterSpacing = 10.sp,
+            fontFamily = FontFamily.Cursive,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+
 }
 
 @Composable
@@ -65,5 +87,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     JetpackComposeTheme {
         Greeting("Android")
+        DisplayOurText()
     }
 }
